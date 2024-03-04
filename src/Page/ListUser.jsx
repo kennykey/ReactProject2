65d97076e3adef9d0b0a1120918ee264a28da025
2201732790 - Kenny Key <61152299+kennykey@users.noreply.github.com>
2024-03-04 22:19:47 +0700

    add pagination

--- src/Page/ListUser.jsx
+++ src/Page/ListUser.jsx
@@ -6,22 +6,55 @@ import { Link } from "react-router-dom"
 
 const ListUser = () =>{
     const [users,setUsers] = useState([])
+    const [pagination, setPagination] = useState({
+        perPage: 0,
+        total: 0,
+        currentPage: 1,
+        previousPage:0,
+        nextPage:0,
+    })
 
     const handleUser = () =>{
         axios
-            .get(`https://reqres.in/api/unknown`)
+            .get(`https://reqres.in/api/unknown?page=${pagination.currentPage}`)
             .then((res) =>{
                 console.log(res.data.data)
                 setUsers(res.data.data)
+                setPagination({
+                    perPage: res.data.per_page,
+                    total: res.data.total,
+                    currentPage: res.data.page,
+                    previousPage: res.data.page - 1,
+                    nextPage: res.data.page + 1,
+                });
             })
             .catch((err) =>{
                 console.log(err)
             })
     }
 
+    const handleBack = () =>{
+        setPagination({
+            ...pagination,
+            currentPage:pagination.previousPage,
+        });
+    };
+
+    const handleNext = () =>{
+        setPagination({
+            ...pagination,
+            currentPage:pagination.nextPage,
+        });
+    };
+
     useEffect(() =>{
         handleUser();
     }, []);
+
+    useEffect(() =>{
+        handleUser();
+    }, [pagination.currentPage]);
+    console.log(pagination);
 
     return(
         <LayOut>
@@ -37,6 +70,14 @@ const ListUser = () =>{
                         </div>
                     </div>
                 ))}
+            </div>
+            <div style={{display:"flex", gap:"30px", alignContent:"center", justifyContent:"center"}} className="py-5">
+                <button onClick={handleBack} disabled={pagination.previousPage === 0} style={{padding:"5px 18px", border:"none", color:"white", backgroundColor:"blue"}}>
+                    Prev
+                </button>
+                <button onClick={handleNext} disabled={pagination.nextPage === 0} style={{padding:"5px 18px", border:"none", color:"white", backgroundColor:"blue"}}>
+                    Next
+                </button>
             </div>
         </LayOut>
     )
